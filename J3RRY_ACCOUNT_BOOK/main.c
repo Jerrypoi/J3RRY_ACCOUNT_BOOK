b1761768c47b603ce901e38698f505053bd05087
Jerrypoi <jerry.poi.sz@gmail.com>
2019-11-09 03:43:21 +0800

    minor redo

--- J3RRY_ACCOUNT_BOOK/main.c
+++ J3RRY_ACCOUNT_BOOK/main.c
@@ -7,15 +7,15 @@ sqlite3* db;
 
 int main(int argc, char *argv[]){
 	// Init database connection;
-	db_connect("db.db"); // �������ݿ�����
-	init_db("./db.sql");
-
+	db_connect("db.db"); // 连接到数据库
+	init_db("./db.sql"); // 根据 SQL 文件创建表
+	insertIntoUser(db, 0, "jerry", "passwd", "email");
+	insertIntoTransactionClass(db, 0, "test_class");
+	insertIntoTransactions(db, 0, 0, 2313.1, 1, 1, "1999-06-01");
 	user result = getUserById(db, 1);
 	transaction_class a = getTransactionClassByID(db, 1);
 	transaction hkb = getTransactionByID(db, 1);
-	insertIntoUser(db,0, "jerry","passwd","email");
-	insertIntoTransactionClass(db, 0, "test_class");
-	insertIntoTransactions(db, 0, 0, 2313.1, 1,1, "1999-06-01");
+
     sqlite3_close(db);
     return 0;
 }
